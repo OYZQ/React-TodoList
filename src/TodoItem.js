8add461eaa13baa467f57080d403340f5ee1aca6
oyzq_glb <ouyangzhiqiang98_glb@gmail.com>
2019-08-22 17:51:02 +0800

    22

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,6 +1,5 @@
-import React, {
-    Component
-} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 class TodoItem extends Component {
     constructor(props) {
         super(props);
@@ -9,13 +8,23 @@ class TodoItem extends Component {
     render() {
         const {content} =  this.props ;
         return (
-        <li onClick={this.handleClick}>{content}</li>
+        <div onClick={this.handleClick}>{content}</div>
         )
     }
     handleClick() {
         const {deleteItem,index} = this.props;
         deleteItem(index);
     }
+}
+
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    content: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+    deleteItem: PropTypes.func,
+    index:PropTypes.number
+}
+TodoItem.defaultProps = {
+    test: 'hello world'
 }
 
 export default TodoItem;
